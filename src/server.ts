da18a06f6789a063dd8543cf17c15ca9d6db8ae3
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-09-10 17:57:49 -0300

    Instala cors e conecta Back e Front

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 console.clear(); // limpa o console
 
 import express from "express"; // importa express da biblioteca express
+import cors from "cors";
 import { mongoose } from "./config/database"; // importa mongoose do arquivo informado
 import { router } from "./config/routes";
 
 const app = express(); // cria a aplicação apartir do express e atribui a constante app
 const db = mongoose; // atribui o mongoose a constante db
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3000, () => {   // roda a aplicação na porta definida, no caso 1234
+app.listen(3000, () => {   // roda a aplicação na porta definida, no caso 3000
     console.log("O servidor está rodando...");
 });
